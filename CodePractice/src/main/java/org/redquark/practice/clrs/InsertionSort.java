226c1f015f4db9f1743ba93439373e90a8b1e456
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-11-22 10:46:22 +0530

    Updated code for insertion sort to include reverse sorting

--- CodePractice/src/main/java/org/redquark/practice/clrs/InsertionSort.java
+++ CodePractice/src/main/java/org/redquark/practice/clrs/InsertionSort.java
@@ -20,21 +20,42 @@ public class InsertionSort {
         return A;
     }
 
+    private static int[] reverseSort(int[] A) {
+        if (A == null || A.length == 0) {
+            return A;
+        }
+        for (int i = 1; i < A.length; i++) {
+            int key = A[i];
+            int j = i - 1;
+            while (j >= 0 && A[j] < key) {
+                A[j + 1] = A[j];
+                j--;
+            }
+            A[j + 1] = key;
+        }
+        return A;
+    }
+
     public static void main(String[] args) {
         int[] A = new int[]{5, 2, 4, 6, 1, 3};
-        System.out.println(Arrays.toString(sort(A)));
+        System.out.println("Non-decreasing: " + Arrays.toString(sort(A)));
+        System.out.println("Non-increasing " + Arrays.toString(reverseSort(A)));
 
         A = new int[]{};
-        System.out.println(Arrays.toString(sort(A)));
+        System.out.println("Non-decreasing: " + Arrays.toString(sort(A)));
+        System.out.println("Non-increasing " + Arrays.toString(reverseSort(A)));
 
         A = new int[]{1, 2, 3, 4, 5, 6};
-        System.out.println(Arrays.toString(sort(A)));
+        System.out.println("Non-decreasing: " + Arrays.toString(sort(A)));
+        System.out.println("Non-increasing " + Arrays.toString(reverseSort(A)));
 
         A = new int[]{5, 4, 5, 4, 2, 3, 1, 2, 1, 4};
-        System.out.println(Arrays.toString(sort(A)));
+        System.out.println("Non-decreasing: " + Arrays.toString(sort(A)));
+        System.out.println("Non-increasing " + Arrays.toString(reverseSort(A)));
 
         A = new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE};
-        System.out.println(Arrays.toString(sort(A)));
+        System.out.println("Non-decreasing: " + Arrays.toString(sort(A)));
+        System.out.println("Non-increasing " + Arrays.toString(reverseSort(A)));
 
     }
 }
