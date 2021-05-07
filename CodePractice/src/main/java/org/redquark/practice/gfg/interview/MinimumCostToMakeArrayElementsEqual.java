0c7a676ce0f577c0769aacef220a54a882d7e141
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-05-07 12:05:08 +0530

    Added code for Minimum Cost To Make Array Elements Equal

---
+++ CodePractice/src/main/java/org/redquark/practice/gfg/interview/MinimumCostToMakeArrayElementsEqual.java
@@ -0,0 +1,30 @@
+package org.redquark.practice.gfg.interview;
+
+/**
+ * @author Anirudh Sharma
+ * <p>
+ * Given an array which contains integer values, we need to make all values of this array equal to some
+ * integer value with minimum cost where the cost of changing an array value x to y is abs(x-y).
+ */
+public class MinimumCostToMakeArrayElementsEqual {
+
+    private static int findCost(int[] nums) {
+        // Length of the array
+        int n = nums.length;
+        int y = n % 2 == 1 ? nums[n / 2] : (nums[n / 2] + nums[(n - 1) / 2]) / 2;
+        // Total cost
+        int cost = 0;
+        for (int x : nums) {
+            cost += Math.abs(x - y);
+        }
+        return cost;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 100, 101};
+        System.out.println(findCost(nums));
+
+        nums = new int[]{4, 6};
+        System.out.println(findCost(nums));
+    }
+}
