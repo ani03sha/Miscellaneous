a64a242adf7ffd51855409bdea4969c95070c97a
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-05-07 19:42:46 +0530

    Added code for Subarray Sums Divisible By K

---
+++ CodePractice/src/main/java/org/redquark/practice/gfg/interview/SubarraySumsDivisibleByK.java
@@ -0,0 +1,34 @@
+package org.redquark.practice.gfg.interview;
+
+/**
+ * @author Anirudh Sharma
+ * <p>
+ * Given an array A of integers, return the number of (contiguous, non-empty) subarrays that have a sum divisible by K.
+ */
+public class SubarraySumsDivisibleByK {
+
+    private static int subarraysDivByK(int[] A, int K) {
+        // Array to store the modulus of the cumulative sum
+        int[] modulus = new int[K];
+        // Cumulative sum
+        int sum = 0;
+        // Loop through the array
+        for (int a : A) {
+            sum += (a % K + K) % K;
+            modulus[(sum % K)]++;
+        }
+        // Variable to store the result
+        int count = modulus[0];
+        // Loop through the modulus array
+        for (int m : modulus) {
+            count += m * (m - 1) / 2;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] A = new int[]{4, 5, 0, -2, -3, 1};
+        int K = 5;
+        System.out.println(subarraysDivByK(A, K));
+    }
+}
