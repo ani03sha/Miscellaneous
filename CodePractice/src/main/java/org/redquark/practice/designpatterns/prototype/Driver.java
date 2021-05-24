748dc9544dfd5a8081084a4ae23124dfa92aff6f
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-05-24 21:54:24 +0530

    Added code for Deep Copy

--- CodePractice/src/main/java/org/redquark/practice/designpatterns/prototype/Driver.java
+++ CodePractice/src/main/java/org/redquark/practice/designpatterns/prototype/Driver.java
@@ -22,5 +22,23 @@ public class Driver {
         System.out.println(secondStatement.getSql());
         System.out.println(secondStatement.getParameters());
         System.out.println(secondStatement.getRecord());
+
+        // Working with registry - deep copy
+        Registry registry = new Registry();
+        Movie movie = (Movie) registry.createItem("Movie");
+        movie.setTitle("Prototype pattern");
+
+        System.out.println(movie);
+        System.out.println(movie.getRuntime());
+        System.out.println(movie.getTitle());
+        System.out.println(movie.getUrl());
+
+        Movie anotherMovie = (Movie) registry.createItem("Movie");
+        anotherMovie.setTitle("Gang of Four");
+
+        System.out.println(anotherMovie);
+        System.out.println(anotherMovie.getRuntime());
+        System.out.println(anotherMovie.getTitle());
+        System.out.println(anotherMovie.getUrl());
     }
 }
